4e0c6660759747f84844f0f722653c6534f43261
Facundo Santos <facusantos123@gmail.com>
2023-01-25 10:20:37 -0300

    Restart the project

--- Algebra_TP2/main.cpp
+++ Algebra_TP2/main.cpp
@@ -1,258 +1,232 @@
 #include <iostream>
 #include <cmath>
-#include <ctime>
-#include "Line.h"
 #include "raylib.h"
 
 using namespace std;
 
-int main(void)
+// -- Structs/Enums. --
+
+struct Vector
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+	Vector3 start;
+	Vector3 end;
+};
 
-    InitWindow(screenWidth, screenHeight, "Algebra_TP2");
+// -- Variables. --
 
-    srand(time(NULL));
+const int windowWidth = 800;
+const int windowHeight = 450;
 
-    //Auxiliares para guardar el valor del tercer vector3.
-    float i;
-    float j;
-    float k;
+Vector x; // Ilustra el vector X default.
 
-    //Auxiliares para guardar los resultdos del calculo de las areas.
-    float area = 0;
-    float areaLine1AndLine2 = 0;
+Vector vA;
+Vector vB;
+Vector vC;
 
-    //Auxiliar para guardar el area total.
-    float totalArea = 0;
+Vector3 p1;
+Vector3 p2;
+Vector3 p3;
 
-    float base; // Base del vector3 (1) y vector3 (2) hasta el vector3 (3).
-    float auxHeight = 0; // Auxiliar para calcular la altura.
-    float height = 0; // Altura.
+Camera3D camera;
 
+// -- Funciones. --
+float CalculateTriangleArea(float a, float b, float c); // Calcula el area de un triangulo pasandole la distancia de cada lado.
+float GetDistance(Vector3 point1, Vector3 point2); // Devuelve la distancia entre 2 puntos en 3D
 
-    float baseLine1AndLine2; // Base desde el vector3 (1) hasta el vector3 (2).
-    float auxHeightLine1AndLine2 = 0; // Auxiliar para calcular la altura del vector3 (1) y del vector3 (2).
-    float heightLine1AndLine2 = 0; // Altura del vector3 (1) y el vector3 (2).
+void CalculateArea(); // Se ocupa de calcular el perimetro y area de las caras.
+void SliceVectors(); // Agarra al vector mas bajo y corta a los demas a esa altura.
 
-    //Se crean las 3 lineas.
-    Line line;
-    Line line2;
-    Line line3;
+// Se crean los 3 vectores.
+void CreateThirdVector();  // Crea el tercer vector haciendo producto cruz de los anteriores dos vectores.
+void CreateSecondVector(); // Crea el segundo vector a 90 grados del primero.
+void CreateFirstVector();  // Crea el primer vector de forma random.
 
-    //Se hace un ramdon para sacar los valores del vetor3 (1).
-    line.x = (rand() % 100 + 1.1) / 10;
-    line.y = (rand() % 100 + 1.1) / 10;
-    line.z = (rand() % 100 + 1.1) / 10;
+void InitVectors(); // Se ocupa de inicializar los vectores.
 
-    //Se declaran los valores del vetor3 (2).
-    line2.x = line.y;
-    line2.y = line.x * -1; //Para que este a 90 grados del primer vector3.
-    line2.z = line.z;
+// Basico
+void Draw();
+void Update();
+void Loop();
+void Init();
 
-    //Se declaran los valores del vector3 (3).
-    line3.x = 0;
-    line3.y = 0;
-    line3.z = 0;
-    //----------------------------------------------------------------------------------
+// -- Main. --
+int main() {
+	InitWindow(windowWidth, windowHeight, "Algebra EJ2");
+	Init();
+	while (!WindowShouldClose()) {
+		Loop();
+	}
+	CloseWindow();
 
-    while (!WindowShouldClose())
-    {
-        //----------------------------------------------------------------------------------
-        
-        //Calculo el largo de la linea desde el vector3{0,0,0} hasta el vector3 (1).
-        //Teorema de Pitagoras. 
-        line.length = sqrt(pow(0 - 0 - 0, 2) + pow(line.x - line.y - line.z, 2));
-
-
-        //Calculo el largo de la linea desde el vector3{0,0,0} hasta el vector3 (2).
-        //Teorema de Pitagoras. 
-        line2.length = sqrt(pow(0 - 0 - 0, 2) + pow(line2.x - line2.y - line2.z, 2));
-        //----------------------------------------------------------------------------------
-
-        //Se crean los vectores para dibujar el grafico del producto cruz.
-        Vector3 vector3LineStart;
-        Vector3 vector3LineEnd;
-
-        Vector3 vector3Line2Start;
-        Vector3 vector3Line2End;
-
-        Vector3 vector3Line3Start;
-        Vector3 vectore3Line3End;
-
-        //Punto de inicio de la primera linea.
-        vector3LineStart.x = 300;
-        vector3LineStart.y = 900;
-        vector3LineStart.z = 0;
-
-        //Punto final de la primera linea.
-        vector3LineEnd.x = line.x *2 + 500;
-        vector3LineEnd.y = line.y*2 + 900;
-        vector3LineEnd.z = 0;
-        //----------------------------------------------------------------------------------
-
-        //Punto de inicio de la segunda linea.
-        vector3Line2Start.x = 300;
-        vector3Line2Start.y = 900;
-        vector3Line2Start.z = 0;
-
-        //Punto final de la segunda linea.
-        vector3Line2End.x = line2.x*2 + 300;
-        vector3Line2End.y = line2.y*2 + 700;
-        vector3Line2End.z = 0;
-        //----------------------------------------------------------------------------------
-
-        //Punto de inicio de la tercera linea.
-        vector3Line3Start.x = 300;
-        vector3Line3Start.y = 900;
-        vector3Line3Start.z = 0;
-
-        //Punto final de la tercera linea.
-        vectore3Line3End.x = line3.x*2 + 400;
-        vectore3Line3End.y = line3.y*2 + 800;
-        vectore3Line3End.z = 0;
-        //----------------------------------------------------------------------------------
-
-        // Update
-
-        //Calculo para sacar el producto cruz
-        i = line.y * line2.y - line.z * line2.y;
-        j = line.z * line2.x - line.x * line2.z;
-        k = line.x * line2.y - line.y * line2.x;
-
-        //El resultado se iguala al nuevo vector3
-        line3.x = i;
-        line3.y = j;
-        line3.z = k;
-        //----------------------------------------------------------------------------------
-
-        //Se obtiene la ditancia segun la linea mas corta.
-        if (line.x < line2.x)
-        {
-            line3.distance = line.x;
-        }
-
-        if (line2.x < line.x)
-        {
-            line3.distance = line2.x;
-        }
-        
-        //----------------------------------------------------------------------------------
-
-        //Base
-        
-        //Para obtener la base de las caras de la piramide que son iguales.
-        //Si la primera linea es la mas corta, se obtiene la base trasando el corte desde el vector3 (1) hasta el vector3 (3). 
-        if (line.length < line2.length)
-        {
-            base = sqrt(pow(line.x - line.y - line.z, 2) + pow(line3.x - line.y - line3.z, 2));
-        }
-
-        //Si la segunda linea es la mas corta, se obtiene la base trasando el corte desde el vector3 (2) hasta el vector3 (3). 
-        if (line2.length < line.length)
-        {
-            base = sqrt(pow(line2.x - line2.y - line2.z, 2) + pow(line3.x - line2.y - line3.z, 2));
-        }
-
-        //Si la primera linea y la segunda son igules, se obtiene la base trasando el corte desde el vector3 (1) hasta el vector3 (3).
-        else
-        {
-            base = sqrt(pow(line.x - line.y - line.z, 2) + pow(line3.x - line.y - line3.z, 2));
-        }
-
-
-        //Para obtener la base de la cara restante.
-        //Se calcula la dstanicia entre el vector3 (1) y el vector3 (2).
-        baseLine1AndLine2 = sqrt(pow(line.x - line.y - line.z, 2) + pow(line2.x - line2.y - line.z, 2));
-
-        //----------------------------------------------------------------------------------
-        
-        //Altura
-
-        //Para calcular la altura de las dos caras que son iguales.
-        //Se calcula el punto en el medio de la base dividiendola por dos.
-        auxHeight = (auxHeight + base) / 2;
-
-        //Luego se saca la distancia desde ese punto hasta el punto 0 para obtener la altura.
-        //Como ambas caras miden lo mismo la altura es la misma.
-        height = sqrt(pow(0, 2) + pow(auxHeight, 2));
-
-
-        //Para calcular la altura de la cara restante.
-        //Se calcula el punto en el medio de la base dividiendola por dos.
-        auxHeightLine1AndLine2 = (auxHeightLine1AndLine2 + baseLine1AndLine2) / 2;
-
-        //Luego se saca la distancia desde ese punto hasta el punto 0 para obtener la altura.S
-        heightLine1AndLine2 = sqrt(pow(0, 2) + pow(auxHeightLine1AndLine2, 2));
-
-        //----------------------------------------------------------------------------------
-        
-        //Area
-
-        //Para calcular el area se utiliza la formula Base * Altura / 2.
-        //Se calcula la base de las dos caras que son iguales.
-        area = (base * height) / 2;
-
-        //Se calcula la base de la cara restante.
-        areaLine1AndLine2 = (baseLine1AndLine2 * heightLine1AndLine2) / 2;
-
-        //Se suman todas las areas y se optiene el area total de la piramide.
-        totalArea = area + area + areaLine1AndLine2;
-
-        //----------------------------------------------------------------------------------
-
-        BeginDrawing();
-
-        ClearBackground(BLACK);
-
-        DrawText("Producto Cruz", GetScreenWidth() /2.4, GetScreenHeight() /8, 60, GREEN);
-
-        //Dibuja los parametros del primer vector3.
-
-        DrawText(TextFormat("X: %02f", line.x), GetScreenWidth() / 10, GetScreenHeight() / 4, 30, RED);
-        DrawText(TextFormat("Y: %02f", line.y), GetScreenWidth() / 10, GetScreenHeight() / 3, 30, RED);
-        DrawText(TextFormat("Z: %02f", line.z), GetScreenWidth() / 10, GetScreenHeight() / 2.4, 30, RED);
-
-        //Dibuja los parametros del segundo vector3.
-
-        DrawText(TextFormat("X: %02f", line2.x), GetScreenWidth() / 5, GetScreenHeight() / 4, 30, YELLOW);
-        DrawText(TextFormat("Y: %02f", line2.y), GetScreenWidth() / 5, GetScreenHeight() / 3, 30, YELLOW);
-        DrawText(TextFormat("Z: %02f", line2.z), GetScreenWidth() / 5, GetScreenHeight() / 2.4, 30, YELLOW);
-
-        DrawText(" = ", GetScreenWidth() / 3.4, GetScreenHeight() / 3, 60, WHITE);
-
-        //Dibuja los parametros del tercer vector3.
-
-        DrawText(TextFormat("X: %02f", line3.x), GetScreenWidth() / 3, GetScreenHeight() / 4, 30, SKYBLUE);
-        DrawText(TextFormat("Y: %02f", line3.y), GetScreenWidth() / 3, GetScreenHeight() / 3, 30, SKYBLUE);
-        DrawText(TextFormat("Z: %02f", line3.z), GetScreenWidth() / 3, GetScreenHeight() / 2.4, 30, SKYBLUE);
-
-        //Dibuja la distancia.
-
-        DrawText(TextFormat("Distance: %01f", line3.distance), GetScreenWidth() / 5, GetScreenHeight() / 2, 30, WHITE);
-
-        //Dibuja el area total.
-
-        DrawText(TextFormat("Total Area: %02f", totalArea), GetScreenWidth() / 1.5, GetScreenHeight() / 3, 50, WHITE);
-
-        //Dibuja las lineas del prducto cruz.
-
-        DrawLine3D(vector3LineStart, vector3LineEnd, RED);
-        DrawLine3D(vector3Line2Start, vector3Line2End, YELLOW);
-        DrawLine3D(vector3Line3Start, vectore3Line3End, SKYBLUE);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    CloseWindow();      
-    //--------------------------------------------------------------------------------------
-
-    return 0;
+	return 0;
 }
 
+float CalculateTriangleArea(float a, float b, float c) {
+	/*
+	Resuelto usando la formula de Heron
+	*/
+	float s = ((a + b + c) / 2);
+	return sqrt(s * ((s - a) * (s - b) * (s - c)));
+}
 
+float GetDistance(Vector3 point1, Vector3 point2) {
+	/*
+	Teorema de Pitagoras / Euclidean distance
+	*/
+	return pow((pow((point2.x - point1.x), 2) + pow((point2.y - point1.y), 2) + pow((point2.z - point1.z), 2)), 0.5);
+}
 
+void CalculateArea() {
+	Vector3 p0 = { 0, 0, 0 };
+
+	float d1 = GetDistance(p1, p3);
+	float d2 = GetDistance(p3, p0);
+	float d3 = GetDistance(p0, p1);
+	cout << "Lados del triangulo A: (" << d1 << ", " << d2 << ", " << d3 << ")\n";
+	float area1 = CalculateTriangleArea(d1, d2, d3);
+	cout << "El area es: " << area1 << "\n";
+	cout << "\n";
+	d1 = GetDistance(p1, p2);
+	d2 = GetDistance(p2, p0);
+	d3 = GetDistance(p0, p1);
+	cout << "Lados del triangulo B: (" << d1 << ", " << d2 << ", " << d3 << ")\n";
+	float area2 = CalculateTriangleArea(d1, d2, d3);
+	cout << "El area es: " << area2 << "\n";
+	cout << "\n";
+	d1 = GetDistance(p2, p3);
+	d2 = GetDistance(p3, p0);
+	d3 = GetDistance(p0, p2);
+	cout << "Lados del triangulo C: (" << d1 << ", " << d2 << ", " << d3 << ")\n";
+	float area3 = CalculateTriangleArea(d1, d2, d3);
+	cout << "El area es: " << area3 << "\n";
+	cout << "\n";
+	cout << "El area total es: " << (area1 + area2 + area3) << "!\n";
+}
+
+void SliceVectors() {
+
+	if (vA.end.y < vB.end.y) {
+		p1 = vA.end;
+
+		p2.y = p1.y;
+		p2.x = (vB.end.x / vB.end.y) * p2.y;
+		p2.z = (vB.end.z / vB.end.y) * p2.y;
+
+		p3.y = p1.y;
+		p3.x = (vC.end.x / vC.end.y) * p2.y;
+		p3.z = (vC.end.z / vC.end.y) * p2.y;
+	}
+	else /*if (vB.end.y < vA.end.y)*/ {
+		p1 = vB.end;
+
+		p2.y = p1.y;
+		p2.x = (vA.end.x / vA.end.y) * p2.y;
+		p2.z = (vA.end.z / vA.end.y) * p2.y;
+
+		p3.y = p1.y;
+		p3.x = (vC.end.x / vC.end.y) * p2.y;
+		p3.z = (vC.end.z / vC.end.y) * p2.y;
+	}
+}
+
+void CreateThirdVector() {
+	//Usamos el producto cruz para crear el 3er vector a 90 grados de los dos vectores ya existentes
+
+	vC.end.x = ((vA.end.y * vB.end.z) - (vA.end.z * vB.end.y));  //Tapas la columna x para averiguarlo 
+	vC.end.y = ((vA.end.z * vB.end.x) - (vA.end.x * vB.end.z));  //Tapas la columna y para averiguarlo 
+	vC.end.z = ((vA.end.x * vB.end.y) - (vA.end.y * vB.end.x));  //Tapas la columna z para averiguarlo 
+}
+
+void CreateSecondVector() {
+	vB.start.x = 0;
+	vB.start.y = 0;
+	vB.start.z = 0;
+	vB.end.x = -vA.end.y;
+	vB.end.y = vA.end.x;
+	vB.end.z = vA.end.z;
+}
+
+void CreateFirstVector() {
+	vA.start.x = 0;
+	vA.start.y = 0;
+	vA.start.z = 0;
+	vA.end.x = GetRandomValue(1, 5);
+	vA.end.y = GetRandomValue(1, 5);
+	vA.end.z = GetRandomValue(-5, -1);
+}
+
+void InitVectors() {
+	x.start.x = -10;
+	x.start.y = 0;
+	x.start.z = 0;
+
+	x.end.x = 10;
+	x.end.y = 0;
+	x.end.z = 0;
+
+	cout << "\n";
+
+	CreateFirstVector();
+	cout << "Vector A: (X: " << vA.end.x << ", Y: " << vA.end.y << ", Z: " << vA.end.z << ")\n";
+	CreateSecondVector();
+	cout << "Vector B: (X: " << vB.end.x << ", Y: " << vB.end.y << ", Z: " << vB.end.z << ")\n";
+	CreateThirdVector();
+	cout << "Vector C: (X: " << vC.end.x << ", Y: " << vC.end.y << ", Z: " << vC.end.z << ")\n";
+	cout << "\n";
+
+	SliceVectors();
+	cout << "Point 1: (X: " << p1.x << ", Y: " << p1.y << ", Z: " << p1.z << ")\n";
+	cout << "Point 2: (X: " << p2.x << ", Y: " << p2.y << ", Z: " << p2.z << ")\n";
+	cout << "Point 3: (X: " << p3.x << ", Y: " << p3.y << ", Z: " << p3.z << ")\n";
+
+	cout << "\n";
+	CalculateArea();
+}
+
+// --
+
+void Draw() {
+	BeginDrawing();
+	ClearBackground(BLACK);
+
+	BeginMode3D(camera); // 3D
+
+	// Ilustracion de X.
+	DrawLine3D(x.start, x.end, RED);
+
+	// Mis tres vectores generados random.
+	DrawLine3D(vA.start, vA.end, ORANGE);
+	DrawLine3D(vB.start, vB.end, PURPLE);
+	DrawLine3D(vC.start, vC.end, YELLOW);
+
+	DrawSphere(p1, 0.2, BLUE);
+	DrawSphere(p2, 0.2, BLUE);
+	DrawSphere(p3, 0.2, BLUE);
+
+	EndMode3D();
+	EndDrawing();
+}
+
+void Update() {
+	UpdateCamera(&camera);
+}
+
+void Loop() {
+	Update();
+	Draw();
+}
+
+void Init() {
+	// Init camara.
+	SetTargetFPS(30);
+
+	camera.position = Vector3{ 0.0f, 0.0f, 20.0f }; // Posicionada apuntando al angulo YX.
+	camera.target = Vector3{ 0.0f, 5.0f, 0.0f };
+	camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+	camera.fovy = 45.0f;
+	camera.projection = CAMERA_PERSPECTIVE;
+
+	SetCameraMode(camera, CAMERA_ORBITAL);
+
+	InitVectors();
+}
